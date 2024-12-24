d48bec46479a93967efd5a9f44b4bb5b4f18d99a
jpv8888 <jpv8888@gmail.com>
2024-12-23 19:40:31 -0500

    2024-12-23

---
+++ params.m
@@ -0,0 +1,11 @@
+% return parameters
+
+function out = params(in)
+    
+    d = dictionary();
+
+    d('fijiScriptsDir') = 'C:\Users\jpv88\Fiji.app\scripts';
+
+    out = d(in);
+   
+end
---
+++ utils/process_final.m
@@ -0,0 +1,41 @@
+function process_final(file)
+   
+    [filepath,name,ext] = fileparts(file);
+    orig_file = strcat(name,ext);
+
+    newStr = split(name,"__");
+    slice = newStr{1};
+    channels = newStr{2};
+    channels = split(channels,"_");
+
+    [filepath,~,~] = fileparts(filepath);
+    [~,name,~] = fileparts(filepath);
+    newStr = split(name,"_");
+    round = newStr{end};
+
+    for i=1:length(channels)
+        channels{i} = strcat(round,'_',channels{i},'.tif');
+    end
+   
+    [filepath,~,~] = fileparts(filepath);
+    savepath = fullfile(filepath,'final',slice);
+
+    ImageJ
+
+    ij.IJ.run("Bio-Formats", strcat("open=",file) + ...
+        " color_mode=Default open_all_series " + ...
+        "rois_import=[ROI manager] " + ...
+        "view=Hyperstack " + ...
+        "stack_order=XYCZT")
+
+    ij.IJ.run("Subtract Background...", "rolling=100");
+    ij.IJ.run("Split Channels");
+    
+    for i=length(channels):-1:1
+        ij.IJ.saveAs("Tiff",fullfile(savepath,channels{i}))
+        ij.IJ.run("Close");
+    end
+
+    ij.IJ.run("Quit","");
+
+end
