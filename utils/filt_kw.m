6d0ead6ccd391561471e2f8a43d12a65830d2db1
jpv8888 <jpv8888@gmail.com>
2025-01-24 17:32:01 -0500

    2025-1-24

--- utils/filt_kw.m
+++ utils/filt_kw.m
@@ -1,13 +1,20 @@
-% input = cell array of strings and string array of keywords
-% filters out strings in the input cell array that contain any of the
+% strs = cell array of strings to be filtered 
+% kw = string array of keywords to look for in strs
+% mode = 'keep' or 'remove' depending on desired filtering
+% removes or keeps strings in the input cell array that contain any of the
 % input keywords
 
-function strs_filt = filt_kw(strs, kw)
+function strs_filt = filt_kw(strs, kw, mode)
 
-    % strings to ignore
+    % which strings contain the keyword
     mask = contains(strs,kw,'IgnoreCase',true);
     
-    % keep the strings that don't have any of the keywords
-    strs_filt = strs(~mask);
+    % filter appropriately depending on desired filtering mode 
+    switch mode
+        case 'keep'
+            strs_filt = strs(mask);
+        case 'remove'
+            strs_filt = strs(~mask);
+    end
    
 end
