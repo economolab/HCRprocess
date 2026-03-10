d13bcb070e5cc2bcf6c1cd917b9a04ef9a559159
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2026-03-10 13:07:37 -0400

    changing local dirs

--- params.m
+++ params.m
@@ -5,7 +5,7 @@ function out = params(in)
     d = dictionary();
     
     d('abcDir') = 'C:\Users\jpv88\Documents\allen-brain-cell-atlas';
-    d('fijiScriptsDir') = 'C:\Users\jpv88\Fiji.app\scripts';
+    d('fijiScriptsDir') = 'C:\Users\ECONOM~1\Desktop\Fiji.app\scripts';
     d('HCRprocessEnv') = 'HCRprocess';
 
     out = d(in);
