dcf2c2c1503fdb0d53e338fe1c44fce72cd4bcc5
Sam Krick <160279286+samkrick25@users.noreply.github.com>
2026-05-29 14:36:04 -0400

    Update outsideBounds.m

--- outsideBounds.m
+++ outsideBounds.m
@@ -1,18 +1,5 @@
 function outsideBounds(fig)
     h = guidata(fig);
-    %h.ax, 'Units', 'normalized', 'Position', [0.0272 0.0576 0.55 0.853]);
-    % ax = get(fig, 'CurrentAxes');
-    % if isempty(ax), return; end
-    % 
-    % cp = get(ax, 'CurrentPoint');
-    % x = round(cp(1,1));
-    % y = round(cp(1,2));
-    % 
-    % [rows, cols, ~] = size(h.stack);
-    % 
-    % if x < 1 || x > cols || y < 1 || y > rows
-    %     fig.WindowButtonMotionFcn = @(f,~)updatePixelIntensity(f);
-    % end
     
     cp = get(h.fig, 'CurrentPoint');
     x = round(cp(1,1));
